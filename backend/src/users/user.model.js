98c2dc7b7cae8806970c0cf881838aea42c3ea1a
nh246 <nazmul210130@diit.edu.bd>
2025-02-10 20:24:58 +0600

    added code

--- backend/src/users/user.model.js
+++ backend/src/users/user.model.js
@@ -1,17 +1,28 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new Schema({
-    username: {type: String, required: true, unique: true},
-    email: {type: String, required: true, unique: true},
-    password: {type: String, required: true},
-    profileImage: String,
-    bio: {type: String, maxLength:200},
-    profession: String,
-    role: {type: String,default: "user", required: true},
-    createdAt: {type: Date, default: Date.now},
-  });
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  profileImage: String,
+  bio: { type: String, maxLength: 200 },
+  profession: String,
+  role: { type: String, default: "user", required: true },
+  createdAt: { type: Date, default: Date.now },
+});
 
+// hash password
 
-  const User = model('User', userSchema);
+userSchema.pre("save", async function (next) {
+  const user = this;
+  if (!user.markModified("password")) return next();
 
-module.exports =User;
+  const hashedPassword = await bcrypt.hash(user.password, 10);
+  user.password = hashedPassword;
+  next();
+});
+
+const User = model("User", userSchema);
+
+module.exports = User;
