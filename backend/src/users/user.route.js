f6c7426085f262224e3218bd42039ccba237d21c
nh246 <nazmul210130@diit.edu.bd>
2025-02-17 21:49:28 +0600

    admin varification

--- backend/src/users/user.route.js
+++ backend/src/users/user.route.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { userRegistration, userLoggedIn, userLogout, getAllUsers } = require("./user.controller");
 const varifyToken = require("../middleware/varifyToken");
+const varifyAdmin = require("../middleware/varifyAdmin");
 
 const router = express.Router();
 
@@ -19,7 +20,7 @@ router.post('/logout', userLogout)
 
 // get all users endpoints (token varify and admin)
 
-router.get('/users', varifyToken, getAllUsers)
+router.get('/users', varifyToken, varifyAdmin, getAllUsers)
 
 
 
