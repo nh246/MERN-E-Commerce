61c9386f6a1a755adc331c91967d90554f3a8b4b
nh246 <nazmul210130@diit.edu.bd>
2025-03-09 21:13:06 +0600

    adding code

--- backend/src/products/product.controller.js
+++ backend/src/products/product.controller.js
@@ -1,9 +1,11 @@
 const Reviews = require("../reviews/review.model");
+import { fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 const { errorResponse, successResponse } = require("../utilis/responseHandler");
 const Products = require("./product.model");
 
 
-// creating a new product
+// creating a new product (admin)
+
 const createNewProduct = async (req, res) => {
   try {
     const newProduct = new Products({ ...req.body });
@@ -31,4 +33,36 @@ const createNewProduct = async (req, res) => {
   }
 };
 
-module.exports = { createNewProduct };
+
+// get All Products
+
+const getAllProducts = async (req,res) => {
+try {
+
+  const {category, color, minPrice, maxPrice, page=1, limit=10} = req.query
+  const filter= {}
+
+  if(category && category !== "all"){
+    filter.category = category
+  }
+
+  if(color && color !== "all"){
+    filter.color = color
+  }
+
+  if(minPrice && maxPrice){
+    const min = perseFloat(minPrice)
+    const max = parseFloat(maxPrice)
+    if(!isNaN(min)&& !isNaN(max)){
+      filter.price = {$gte:min, $lte:max}
+    }
+  }
+
+  const products = await Products.find()
+  return successResponse(res, 200, "All products fetched successfully", products)
+} catch (error) {
+  return errorResponse(res, 500, " Failed to get all products", error)
+}
+}
+
+module.exports = { createNewProduct , getAllProducts};
--- backend/src/products/product.route.js
+++ backend/src/products/product.route.js
@@ -1,9 +1,18 @@
 const express = require('express')
-const { createNewProduct } = require('./product.controller')
+
+const  verifyToken  = require('../middleware/varifyToken')
+const verifyAdmin = require('../middleware/varifyAdmin')
+
+const { createNewProduct, getAllProducts } = require('./product.controller')
 const router = express.Router()
 
-// Create a product
+// Create a product ( only admin)
 
-router.post("/create-product", createNewProduct)
+router.post("/create-product",verifyToken, verifyAdmin,  createNewProduct)
+
+// get all products
+
+router.get("/", getAllProducts)
+
 
 module.exports = router
