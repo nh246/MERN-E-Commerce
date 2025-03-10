817caa8243aa23fddb7eb4935e8083f8e0493952
nh246 <nazmul210130@diit.edu.bd>
2025-03-10 13:48:26 +0600

    Product update and delete api created

--- backend/src/products/product.controller.js
+++ backend/src/products/product.controller.js
@@ -78,4 +78,68 @@ try {
 }
 }
 
-module.exports = { createNewProduct , getAllProducts};
+// get single product
+
+const getSingleProduct = async (req, res)=> {
+
+  const {id}= req.params
+
+  try {
+  const product = await Products.findById(id).populate("author", "email username")
+
+  if(!product){
+    return errorResponse(res, 404, "Product not found")
+  }
+  const reviews = await Reviews.find({productId: id}).populate("userId", "email username")
+
+  return successResponse(res,200, "Single product and reviews", {product, reviews})
+
+    
+  } catch (error) {
+    return errorResponse(res, 500, "Failed to get single product", error)
+  }
+}
+
+
+// update ProductById
+
+const updateProductById = async (req,res)=> {
+  
+  const productId = req.params.id
+  try {
+    const updatedProduct = await Products.findByIdAndUpdate(productId, {...req.body}, {new:true})
+
+    if(!updatedProduct){
+      return errorResponse(res, 404, "Product not found")
+
+    }
+
+    return successResponse(res, 200, "Product updated successfully", updatedProduct)
+  } catch (error) {
+    return errorResponse(res, 500, "Failed to update product", error)
+  } 
+
+}
+
+// delete Product By Id
+
+const deleteProductById = async (req,res)=> {
+  
+   const productId = req.params.id
+
+   try {
+    const deletedProduct = await Products.findByIdAndDelete(productId)
+
+    if(!deletedProduct){
+      return errorResponse(res, 404, "Product not found")
+    }
+    await Reviews.deleteMany({productId: productId})
+    return successResponse(res, 200, "Product deleted successfully")
+    
+   } catch (error) {
+    return errorResponse(res, 500, "Failed to delete Product", error)
+   }
+}
+
+
+module.exports = { createNewProduct , getAllProducts, getSingleProduct, updateProductById , deleteProductById};
