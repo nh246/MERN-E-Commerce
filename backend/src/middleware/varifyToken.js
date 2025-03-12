f9d3294805195f8394ba4a3d3e091131ad11448b
nh246 <nazmul210130@diit.edu.bd>
2025-03-12 15:36:43 +0600

    reviews api added

--- backend/src/middleware/varifyToken.js
+++ backend/src/middleware/varifyToken.js
@@ -4,8 +4,8 @@ const jwt = require('jsonwebtoken')
 const JWT_SECRET = process.env.JWT_SECRET_KEY
 const varifyToken = (req, res, next)=> {
  try {
-    // const token = req.cookies.token  // TODO: uncomment this when done
-    const token = req.headers.authorization?.split(' ')[1]
+    const token = req.cookies.token  // TODO: uncomment this when done
+    // const token = req.headers.authorization?.split(' ')[1]
     // console.log("Token from cookies", token)
 
     if(!token){
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,13 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authApi from "./features/auth/authApi";
 import authReducer from "./features/auth/authSlice"
+import productsApi from "./features/products/productsApi";
+import reviewsApi from "./features/reviews/reviewsApi";
 
  export const store = configureStore({
     reducer: {
         [authApi.reducerPath]: authApi.reducer,
-        auth: authReducer
+        auth: authReducer,
+        [productsApi.reducerPath]: productsApi.reducer,
+        [reviewsApi.reducerPath]: reviewsApi.reducer,
     },
 
     middleware: (getDefaultMiddleware) =>
-         getDefaultMiddleware().concat(authApi.middleware)
+         getDefaultMiddleware().concat(authApi.middleware, productsApi.middleware, reviewsApi.middleware)
 })
