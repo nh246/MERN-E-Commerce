7ab260740a414d62b3a68b6107281860e5c918f1
nh246 <nazmul210130@diit.edu.bd>
2025-03-20 22:14:26 +0600

    order and stats store done

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -4,6 +4,8 @@ import authReducer from "./features/auth/authSlice"
 import productsApi from "./features/products/productsApi";
 import reviewsApi from "./features/reviews/reviewsApi";
 import cartReducer from "../redux/features/cart/cartSlice"
+import orderApi from "./features/orders/orderApi"
+import statsApi from "./features/stats/statsApi"
 
  export const store = configureStore({
     reducer: {
@@ -12,8 +14,10 @@ import cartReducer from "../redux/features/cart/cartSlice"
         [productsApi.reducerPath]: productsApi.reducer,
         [reviewsApi.reducerPath]: reviewsApi.reducer,
         cart: cartReducer,
+        [orderApi.reducerPath]: orderApi.reducer,
+        [statsApi.reducerPath]: statsApi.reducer,
     },
 
     middleware: (getDefaultMiddleware) =>
-         getDefaultMiddleware().concat(authApi.middleware, productsApi.middleware, reviewsApi.middleware)
+         getDefaultMiddleware().concat(authApi.middleware, productsApi.middleware, reviewsApi.middleware , orderApi.middleware, statsApi.middleware),
 })
