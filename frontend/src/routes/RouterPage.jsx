585e10aa585ae884959423276a0228abd95c0c3a
nh246 <nazmul210130@diit.edu.bd>
2025-03-22 01:13:58 +0600

    User review done

--- frontend/src/routes/RouterPage.jsx
+++ frontend/src/routes/RouterPage.jsx
@@ -13,6 +13,8 @@ import PrivateRoute from "./PrivateRoute";
 import UserDMain from "../pages/dashboard/user/dashboard/UserDMain";
 import UserOrders from "../pages/dashboard/user/orders/UserOrders";
 import OrderDetail from "../pages/dashboard/user/orders/OrderDetail";
+import UserPayments from "../pages/dashboard/user/payments/UserPayments";
+import UserReviews from "../pages/dashboard/user/reviews/UserReviews";
 function RouterPage() {
   return (
     <Routes>
@@ -31,8 +33,8 @@ function RouterPage() {
         <Route index element={<UserDMain/>} />
         <Route path="profile" element={<h1>Profile</h1>} />
         <Route path="orders" element={<UserOrders/>} />
-        <Route path="payments" element={<h1>Payments</h1>}/>
-        <Route path="reviews" element={<h1>reviews</h1>}/>
+        <Route path="payments" element={<UserPayments/>}/>
+        <Route path="reviews" element={<UserReviews/>}/>
         <Route path="admin" element={ <PrivateRoute role={"admin"} ><h1>admin dashboard</h1></PrivateRoute> }/>
         <Route path="add-product" element={<PrivateRoute role={"admin"}><h1>add-product</h1></PrivateRoute>}/>
         <Route path="manage-products" element={<PrivateRoute role={"admin"}><h1>manage-products</h1></PrivateRoute>}/>
